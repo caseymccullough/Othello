bf2ca7be5f463f0c72bf84cb0d39389bc30d3fa2
caseymccullough <kcmccullough@gmail.com>
2021-03-21 15:31:27 -0400

    scoreboard w/ tile counts now working.  Board centered and screen size maximized

--- js/script.js
+++ js/script.js
@@ -2,21 +2,20 @@
 
 let gameBoard = document.getElementById("board");
 
+
 // 
 
 let grid = [];
-let numBlackTiles = 0;
-let numWhiteTiles = 0;
 let currentPlayer;
 
 
 const init = () => {
 
-    grid.push ([1, -1, 0, 0, 0, 0, 0, 0]);
-    grid.push ([-1, 1, 0, 0, 0, 0, 0, 0]);
     grid.push ([0, 0, 0, 0, 0, 0, 0, 0]);
     grid.push ([0, 0, 0, 0, 0, 0, 0, 0]);
     grid.push ([0, 0, 0, 0, 0, 0, 0, 0]);
+    grid.push ([0, 0, 0, 1, -1, 0, 0, 0]);
+    grid.push ([0, 0, 0, -1, 1, 0, 0, 0]);
     grid.push ([0, 0, 0, 0, 0, 0, 0, 0]);
     grid.push ([0, 0, 0, 0, 0, 0, 0, 0]);
     grid.push ([0, 0, 0, 0, 0, 0, 0, 0]);
@@ -29,6 +28,7 @@ const init = () => {
             let square = document.createElement("div");
             
             square.id = "r" + row + "c" + col;
+            square.classList.add("square");
             square.innerText = "r" + row + "c" + col;
 
             square.addEventListener('click', processSquareSelection);
@@ -52,42 +52,53 @@ const processSquareSelection = (event) => {
 }
 
 const updateHTML = () => {
-
-    numBlackTiles = 0;
-    numWhiteTiles = 0;
+    
+    blackTileCount = 0;
+    whiteTileCount = 0;
 
     for (let row = 0; row < grid.length; row++)
     {
         for (let col = 0; col < grid[row].length; col++)
         {
-            changeSquareDivs(row, col);
+            
+            if (grid[row][col] == -1) {
+                makeSquareWhite(row, col);
+                whiteTileCount++;
+            }
+            else if (grid[row][col] == 1)
+            {
+                makeSquareBlack(row,col);
+                blackTileCount++;
+            }
 
         }
 
     } // end nested loops
 
-    console.log("White tiles: " + numWhiteTiles);
-    console.log("Black tiles: " + numBlackTiles);
+    document.getElementById("num-black-tiles").innerHTML = blackTileCount;
+    document.getElementById("num-white-tiles").innerHTML = whiteTileCount;
+    
     currentPlayer *= -1; // flip -1 to +1 and vice versa
     console.log ("current: " + currentPlayer);
 } // end updateHTML()
 
-function changeSquareDivs(row, col) {
-    if (grid[row][col] == -1) {
+function makeSquareWhite (row, col) {
+    
         let squareId = "r" + row + "c" + col;
         let squareToWhite = document.getElementById(squareId);
         squareToWhite.classList.remove("black");
         squareToWhite.classList.add("white");
-        numWhiteTiles++;
-    }
-    else if (grid[row][col] == 1) {
+
+}
+function makeSquareBlack(row, col) {
+    
         let squareId = "r" + row + "c" + col;
         let squareToBlack = document.getElementById(squareId);
         squareToBlack.classList.remove("white");
         squareToBlack.classList.add("black");
-        numBlackTiles++;
-    }
 }
+
+
 
 init();
 
